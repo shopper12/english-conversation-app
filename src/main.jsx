0ed08e18298a724042e626cfe823556a2c33b2ce
j <136126244+shopper12@users.noreply.github.com>
2026-08-08 13:16:55 +0900

    feat: mount human interviewer evaluator

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import InterviewTurnManager from './InterviewTurnManager.jsx'
+import HumanInterviewEvaluator from './HumanInterviewEvaluator.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <InterviewTurnManager />
+    <HumanInterviewEvaluator />
   </React.StrictMode>,
 )
